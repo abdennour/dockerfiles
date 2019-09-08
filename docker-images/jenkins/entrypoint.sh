7072037d37c5a85bd8a4e0568bfe603493ab34ff
abdennour <mail@abdennoor.com>
2019-09-08 07:03:04 +0300

    jenkins Dind

--- docker-images/jenkins/entrypoint.sh
+++ docker-images/jenkins/entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+set -e
 
 # When a DOCKER_GID_ON_HOST is supplied, run jenkins with this
 # group id - to access the docker socket shared via volume
-# dockerGroupName=""
-# if [ -n "$DOCKER_GID_ON_HOST" ]; then
+dockerGroupName=""
+if [ -n "$DOCKER_GID_ON_HOST" ]; then
 
-# echo "Create group for gid $DOCKER_GID_ON_HOST"
-# sudo groupadd -g $DOCKER_GID_ON_HOST docker && sudo grpconv
-# sudo usermod -a  -G $DOCKER_GID_ON_HOST jenkins
-# dockerGroupName=$(cat /etc/group | grep :$DOCKER_GID_ON_HOST: | cut -d: -f1)
+  echo "Create group for gid $DOCKER_GID_ON_HOST"
+  sudo groupadd -g $DOCKER_GID_ON_HOST docker && sudo grpconv
+  sudo usermod -a  -G $DOCKER_GID_ON_HOST jenkins
+  dockerGroupName=$(cat /etc/group | grep :$DOCKER_GID_ON_HOST: | cut -d: -f1)
 
-# fi;
+fi;
 
 #-----
 for i in "$@"
@@ -34,14 +35,18 @@ done
 
 if [ ! -z "${plugins}" ]; then
 
-echo "installing plugins ${plugins}: ..."
+  echo "installing plugins ${plugins}: ..."
 
-/usr/local/bin/install-plugins.sh $(echo ${plugins} | sed "s/,/ /g")
+  /usr/local/bin/install-plugins.sh $(echo ${plugins} | sed "s/,/ /g")
 
 fi;
 
 echo PLUGINS ${plugins};
-echo REST args $@;
-# ENTRYPOINT ["/sbin/tini", "--", "/usr/local/bin/jenkins.sh"]
 
-exec /sbin/tini -- /usr/local/bin/jenkins.sh "$@"
+# ENTRYPOINT ["/sbin/tini", "--", "/usr/local/bin/jenkins.sh"]
+cmdLine=$(/sbin/tini -- /usr/local/bin/jenkins.sh "$@")	
+if [ -n "$dockerGroupName" ]; then 
+  exec sg $dockerGroupName "$cmdLine"
+else
+  exec $cmdLine
+fi
