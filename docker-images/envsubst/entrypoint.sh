dab4977e806b602674d486a9e127668219c423d2
abdennour <mail@abdennoor.com>
2019-10-04 13:55:47 +0300

    fix: build hook

--- docker-images/envsubst/entrypoint.sh
+++ docker-images/envsubst/entrypoint.sh
@@ -4,7 +4,10 @@
 # $0 /path/file
 # $0 < /path/file
 # echo "my content" | $0
-content_to_replace="${1:-/dev/stdin}"
-
-echo -n "${content_to_replace}" | perl -pe 's/\$([_A-Z]+)/$ENV{$1}/g' | perl -pe 's/\$(\{)?([a-zA-Z_]\w*)(?(1)\})/$ENV{$2}/g'
+file_path_or_content="${1:-/dev/stdin}"
+if [[ -f "${file_path_or_content}" ]]; then
+cat file_path_or_content | perl -pe 's/\$([_A-Z]+)/$ENV{$1}/g' | perl -pe 's/\$(\{)?([a-zA-Z_]\w*)(?(1)\})/$ENV{$2}/g'
+else 
+ echo -n "${file_path_or_content}" | perl -pe 's/\$([_A-Z]+)/$ENV{$1}/g' | perl -pe 's/\$(\{)?([a-zA-Z_]\w*)(?(1)\})/$ENV{$2}/g'
+fi
 # Only substitute variable that are defined perl -pe 's{\$(\{)?(\w+)(?(1)\})}{$ENV{$2} // $&}ge'
